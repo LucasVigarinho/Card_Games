2e23cab95b18b6aeb300e578cc790eeae1687e90
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-06-20 21:20:51 +0100

    Commit - testing testing

--- CardsHiLo_Game_Application_JavaFX/src/hilo_game/package-info.java
+++ CardsHiLo_Game_Application_JavaFX/src/hilo_game/package-info.java
@@ -1,6 +1,36 @@
 /**
+ * /** 
  * 
- */
+ * (a) Create a project called 'CardsHiLoGUI'. Create a main interface for the
+ * application similar to that shown in Figure 1. Remember to create a ToggleGroup
+ * for the radio buttons and set the ToggleGroup for each button to ensure mutual
+ * exclusivity. At application startup, the card images are populated with default
+ * cards. Use comments in your code to explain your approach in relation to the GUI
+ * construction. 
+ * 
+ * (b) Add a menu bar and 'File' menu. The 'File' menu should contain the items 'New
+ * Game', 'Shuffle' and 'Exit'. Implement these menu items fully. Add a 'Help' menu
+ * with an 'About' item to show your student name and number in a dialog.
+ * 
+ * (c) Create classes for Card and DeckOfCards to help manage the game. The Card
+ * class should include methods such as isEqual( ), isLower( ), isHigher( ) and
+ * toString( ). The class DeckOfCards should include methods such as
+ * dealTopCard( ), isEmpty( ) and shuffle( ). Provide one or more constructors as
+ * appropriate for each class.
+ * 
+ * (d) Implement dealing of each card, determination of a win/lose and user feedback in
+ * a label as shown in Figure 1. Store wins and inform the user of an overall game
+ * win on achieving 5 consecutive, correct guesses.
+ * 
+ * (e) Add a progress bar and progress indicator to indicate progress towards 5
+ * consecutive wins leading to an overall game win. Provide appropriate feedback on
+ * achieving an overall game win. Use a stylesheet to apply an attractive look and
+ * feel to the application.
+ * 
+ * 
+ * @author Lucas Maximo 
+ * */
+ 
 /**
  * @author lucasmaximo
  *
